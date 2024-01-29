84367d7551e373c03cd36434f28cb5db24c4b47e
Suzukisakae <lethanhvinhspk@gmail.com>
2024-01-29 22:01:14 +0700

    Day 06 - part 1

---
+++ javascript/Day06-Exer01.js
@@ -0,0 +1,7 @@
+// Ký tự không lặp lại - Bạn được cung cấp một chuỗi chỉ bao gồm các bảng chữ cái tiếng Anh viết 
+// thường và viết hoa và các số nguyên từ 0 đến 9. Hãy viết một hàm lấy chuỗi này làm Đầu vào và trả về 
+// chỉ mục của ký tự đầu tiên không lặp lại.
+
+function findNonRepeatingCharacter(string){
+    //write code here
+}
